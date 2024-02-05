b2ff37e7bb9b69a755607d2f933289b323901798
Helias <stefanoborzi32@gmail.com>
2024-02-05 11:11:55 +0100

    fix: lint

--- tests/test_rest_api.py
+++ tests/test_rest_api.py
@@ -4,5 +4,5 @@ from models.constants import HEALTHCHECK_MESSAGE
 
 
 def test_healthcheck() -> None:
-    r = requests.get("http://localhost:5000/healthcheck")
+    r = requests.get("http://localhost:5000/healthcheck", timeout=10)
     assert r.text == HEALTHCHECK_MESSAGE
