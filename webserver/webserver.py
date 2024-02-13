70fdb7fe2b0691a6bae726d773e4fa2926801014
Alex Matter <alessio1512@gmail.com>
2024-02-13 19:52:47 +0100

    localhost -> None on websocket

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -5,7 +5,7 @@ from webserver.websocket import start_websocket
 
 
 def start_socket() -> None:
-    asyncio.run(start_websocket("localhost", 8765))
+    asyncio.run(start_websocket(None, 8765))
 
 def start_rest_api() -> None:
     app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
